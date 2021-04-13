6717fbbcb2b07cee3ecd7ad28bdf3e8eb19c6935
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2021-04-13 13:21:37 -0600

    Update SpriteFactory.java

--- marioai/src/ch/idsia/mario/engine/sprites/spritebuilder/SpriteFactory.java
+++ marioai/src/ch/idsia/mario/engine/sprites/spritebuilder/SpriteFactory.java
@@ -1,22 +1,25 @@
 package ch.idsia.mario.engine.sprites;
 /*
  * The factory class is used to create sprites which are similar to each other
+ * We couldn't convert all the sprites to a factory pattern
  */
 public class SpriteFactory {
 	public Sprite getSpriteType(String spriteType, LevelScene world, float x, float y, int facing) {
 		if(spriteType.equalsIgnoreCase("Bullet Bill")) {
 			return new BulletBill(world, x, y, facing)
 		}
-		// Don't need world arg
+		// Don't need world arg or facing arg
 		else if(spriteType.equalsIgnoreCase("CoinAnim")) {
 			return new CoinAnim(x,y);
 		}
 		else if(spriteType.equalsIgnoreCase("Fireball")) {
 			return new Fireball(world, x, y, facing);
 		}
+		//Doesn't matter which way Fire Flower faces
 		else if(spriteType.equalsIgnoreCase("FireFlower")) {
 			return new FireFlower(world, x, y);
 		}
+		//Doesn't matter which way Mushroom faces
 		else if(spriteType.equalsIgnoreCase("Mushroom")) {
 			return new Mushroom(world, x, y);
 		}
